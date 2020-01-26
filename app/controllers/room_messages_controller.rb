a6c3b1cd9880b69b39f8ce8ab917afc51c20721b
Shivakumara M <shivasorab@gmail.com>
2020-01-26 20:33:48 +0530

    jsutalk implementation

---
+++ app/controllers/room_messages_controller.rb
@@ -0,0 +1,18 @@
+class RoomMessagesController < ApplicationController
+  before_action :load_entities
+  skip_before_action :verify_authenticity_token
+  def create
+    @room_message = RoomMessage.create user: current_user,
+                                       room: @room,
+                                       message: params.dig(:room_message, :message)
+
+   RoomChannel.broadcast_to @room, @room_message
+   redirect_to room_path(@room)
+  end
+
+  protected
+
+  def load_entities
+    @room = Room.find params[:room_message][:room_id]
+  end
+end
